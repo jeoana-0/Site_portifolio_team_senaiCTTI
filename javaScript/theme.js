649972c42b47a5f00b7872fb7a3ea21c5b26092a
Jeoanã <107138188+jeoana-0@users.noreply.github.com>
2022-09-23 15:39:05 -0300

    thema modificado background

--- javaScript/theme.js
+++ javaScript/theme.js
@@ -2,8 +2,19 @@ function change_theme(){
 let checkbox = document.getElementById('theme');
 let el = document.getElementById('body');
 if(checkbox.checked) {
-    el.style.backgroundColor = "white"
+    el.style.background = "var(--gradiente-roxo)"
 }else{
-    el.style.backgroundColor ="var(--CorPrincipal)"
+    el.style.background ="var(--gradiente-roxo-aqua)"
 }
 }
+
+
+// function corBody() {
+//     let body = document.getElementById("body");
+//     body.style.background = "var(--gradiente-roxo)";
+// }
+
+// function voltaCorBody() {
+//     let body = document.getElementById("body");
+//     body.style.background = "var(--gradiente-roxo-aqua)";
+// }
